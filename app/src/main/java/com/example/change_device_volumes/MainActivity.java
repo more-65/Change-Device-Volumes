72e2c65689ecae9604c56b7ca0179bbbc2f40ecd
more-65 <ichigo.sk6@gmail.com>
2020-10-27 00:54:23 +0900

    initial commit

---
+++ app/src/main/java/com/example/change_device_volumes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.change_device_volumes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
